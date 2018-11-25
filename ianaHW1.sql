2e603cb7c1fb41109c7c1376e2535a80e391c12f
YanaMsk <44459586+YanaMsk@users.noreply.github.com>
2018-11-25 23:59:43 +0300

    Add files via upload

---
+++ ianaHW1.sql
@@ -0,0 +1,130 @@
+SELECT ('ФИО:  Иванникова Яна Геннадьевна');
+
+
+-- запрос 1.1 выбрать 10 записей из таблицы rating
+
+SELECT * FROM ratings LIMIT 10;
+
+
+-- запрос 1.2 выбрать из таблицы links всё записи, у которых imdbid оканчивается на "42", а поле movieid между 100 и 1000
+(
+  SELECT * FROM links
+  WHERE 
+      imdbid LIKE '%42'
+      AND
+      movieid >= 100
+)INTERSECT (
+
+  SELECT * FROM links
+    WHERE 
+        imdbid LIKE '%42'
+        AND
+        movieid <= 1000
+)
+LIMIT 10;
+
+-- ИЛИ
+
+SELECT * FROM links
+WHERE 
+      imdbid LIKE '%42'
+      AND
+      movieid BETWEEN 100 and 1000
+LIMIT 10;
+
+
+-- запрос 2.1 выбрать из таблицы links все imdb_id, которым ставили рейтинг 5
+
+SELECT imdbid, rating
+FROM links
+JOIN ratings
+    ON links.movieid=ratings.movieid
+WHERE rating =5
+LIMIT 10;
+
+
+
+-- запрос 3.1 Посчитать число фильмов без оценок
+SELECT COUNT(*)
+FROM public.links
+LEFT JOIN public.ratings
+    ON links.movieid=ratings.movieid
+WHERE ratings.movieid IS NULL;
+
+
+
+-- запрос 3.2 GROUP BY, HAVING вывести top-10 пользователей, у который средний рейтинг выше 3.5.
+
+SELECT userid, AVG(rating)
+FROM ratings
+GROUP BY userid 
+HAVING AVG(rating) > 3.5
+ORDER BY AVG(rating) DESC
+LIMIT 10;
+
+
+
+-- запрос 4.1 достать 10 imbdId из links у которых средний рейтинг больше 3.5 
+
+
+SELECT l.imdbid, t.movieid
+FROM links as l
+JOIN 
+(SELECT movieid, AVG(rating)
+FROM ratings
+GROUP BY movieid
+HAVING AVG(rating) > 3.5) as t
+      on t.movieid=l.movieid
+LIMIT 10;
+
+-- ИЛИ
+
+SELECT imdbid
+FROM links 
+WHERE movieid IN (
+SELECT movieid
+FROM ratings
+GROUP BY movieid
+HAVING AVG(rating) > 3.5 )
+LIMIT 10;
+
+--- Подсчитать средний рейтинг по всем пользователям, которые попали под условие - то есть в ответе должно быть одно число.
+
+SELECT AVG (rating_avr)
+FROM (
+SELECT l.imdbid, t.movieid, rating_avr
+FROM links as l
+JOIN 
+(SELECT movieid, AVG(rating) as rating_avr
+FROM ratings
+GROUP BY movieid
+HAVING AVG(rating) > 3.5) as t
+      on t.movieid=l.movieid
+LIMIT 10) as itog;
+
+
+-- запрос 4.2 - Common Table Expressions: посчитать средний рейтинг по пользователям, у которых более 10 оценок
+
+WITH table_itog
+AS
+(
+
+WITH number_rating_userid
+AS 
+(
+SELECT userid,
+	COUNT (rating) as rating_count,
+	AVG (rating) as rating_avarage
+FROM ratings
+GROUP BY userid, rating
+) 
+SELECT *
+FROM number_rating_userid
+WHERE rating_count > 10
+)
+SELECT AVG (rating_avarage)
+FROM table_itog
+;
+
+
+
